637da0d42423d04384511484b9a0f28b87733f65
Esteban Provoste <eprovoste@summasolutions.net>
2020-11-27 17:55:23 -0300

    Initial commit

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TimeSplit } from './typings/global'
+import { tick } from './utils/time'
+import { useCssHandles } from "vtex.css-handles"
 
-interface CountdownProps { }
+interface CountdownProps {
+  targetDate: string
+ }
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
-  return <div></div>
+ const CSS_HANDLES = ["countdown"]
+
+ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
+  targetDate,
+}) => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+  })
+
+  const handles = useCssHandles(CSS_HANDLES)
+
+  tick(targetDate, setTime)
+
+  return (
+    <div className={`${handles.countdown} db tc`}>
+      {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+    </div>
+  )
 }
 
 Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    targetDate: {
+      title: 'Final date',
+      description: 'Final date used in the countdown',
+      type: 'string',
+      default: null,
+    },
+  },
 }
 
 export default Countdown
